5b6955d86aaba14721524632dd39f16a193de289
oliverDam <oliver-d@hotmail.com>
2018-02-05 12:57:24 +0100

    setup

---
+++ Shit Deleter - Linux (måske mac).sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rm {*.aux,*.bbl,*.bcf,*.bcf,*.blg,*.brf,*.log,*.lox,*.out,*.run.xml,*.synctex.gz,*.tdo,*.toc,*.tps}
---
+++ Shit Deleter.bat
@@ -0,0 +1,17 @@
+echo off
+
+rem the following deletes only files and NOT directories.
+
+del "*.aux"
+del "*.bbl"
+del "*.bcf"
+del "*.blg"
+del "*.brf"
+del "*.log"
+del "*.lox"
+del "*.out"
+del "*.run.xml"
+del "*.synctex.gz"
+del "*.tdo"
+del "*.toc"
+del "*.tps"
